113be92c3a324d7740e6f612d45d320141e7dce6
Pål Thomassen <pal.thomassen@bekk.no>
2024-12-12 20:16:54 +0100

    Legger til achordion igjen

--- 5V766/keymap.c
+++ 5V766/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "i18n.h"
+#include "features/achordion.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
@@ -123,7 +124,43 @@ bool rgb_matrix_indicators_user(void) {
   return true;
 }
 
+void matrix_scan_user(void) {
+  achordion_task();
+}
+
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  // Exceptionally consider the following chords as holds, even though they
+  // are on the same hand in Dvorak.
+  switch (tap_hold_keycode) {
+    case KC_F: 
+      if (other_keycode == KC_TAB) { return true; }
+      break;
+
+    case KC_SPACE:   
+      if (other_keycode == KC_C || other_keycode == KC_V) { return true; }
+      break;
+  }
+
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
+bool achordion_eager_mod(uint8_t mod) {
+  switch (mod) {
+    case MOD_LCTL:
+      return true;  // Eager for Ctrl.
+
+    default:
+      return false;
+  }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
+
   switch (keycode) {
 
     case TD(DANCE_4):
